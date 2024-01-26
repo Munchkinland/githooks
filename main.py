13ce9333d6f81478d8ee278b6272017ea42230a6
gitflamingo <ruben_carrasco@hotmail.com>
2024-01-26 09:18:56 +0000

    pre commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Hello world
 [default]
-aws_access_key_id=
-aws_secret_access_key = KP8RNG2JX6BZ4H7M3F9Y1U5CD
+aws_access_key_id= 
+aws_secret_access_key = KP8RNG2JX6BZ
 
 output = json
 region = us-east-1
